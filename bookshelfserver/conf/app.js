0770acf3a4b776a72e94051f3a50be79e30a7a8d
yangang.li <yangang.li@99bill.com>
2018-06-01 19:29:59 +0800

    项目初始化

---
+++ bookshelfserver/conf/app.js
@@ -0,0 +1,9 @@
+module.exports = {
+    appConfig: {
+        appid: 'wx112902cee8469942',
+        secret: '5d00e7cb8bd86ff3aa794d3e706a1f83'
+    },
+    userConfig: {
+        credit: 30          // 用户初始积分余额
+    }
+}
